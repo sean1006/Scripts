2ccc3a73118e6f5990b6ab24b187ad1442830ab0
Sean Yin <sean999_y@yahoo.com>
2020-01-10 11:16:51 -0800

    initial commit

---
+++ ch-ext.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ch-ext.sh
+# arg1 (from extension), arg2 (to extension)
+
+# PRE-DOWNLOAD
+# sudo apt install rename
+
+if [ $1 ] && [ $2 ] && [ ! $3 ];
+then
+    rename "s/\.$1$/.$2/" *.$1
+    rename "s/\.$1$/.$2/" **/*.$1
+    echo "Finished converting .$1 to .$2"
+else
+    echo "Please input 2 parameters"
+fi
